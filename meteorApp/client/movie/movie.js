31823c59156529328d75829b22efdfbc48d91e2d
Philipp Legner <philipp@mathigon.org>
2014-12-20 21:25:48 +0000

    Updated icon design and position

--- meteorApp/client/movie/movie.js
+++ meteorApp/client/movie/movie.js
@@ -1,5 +1,5 @@
 Template.movie.events = {
-    'click .bookmark' : function () {
+    'click .movie-bookmark' : function () {
       Meteor.users.update({_id : Meteor.userId()}, { $pull : { 'profile.movies.suggested' : this._id }});
       Meteor.users.update({_id : Meteor.userId()}, { $push : { 'profile.movies.starred' : this._id }});
     }
